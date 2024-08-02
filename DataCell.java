22f4edf21f6a77de387453e90286e88c792207b7
2snufkin <pini.tzr@gmail.com>
2024-08-02 11:17:22 +0200

    with border

--- DataCell.java
+++ DataCell.java
@@ -4,6 +4,8 @@ public class DataCell {
     private CellContent cellContent;
     private String borderLeftColor;
     private int colspan = 1;
+
+    private boolean withBorder;
     private AlignmentType alignmentType = AlignmentType.LEFT;
 
     public DataCell(CellContent cellContent) {
@@ -19,6 +21,14 @@ public class DataCell {
         this.cellContent = cellContent;
         this.borderLeftColor = borderLeftColor;
         this.colspan = colspan;
+        this.alignmentType = alignmentType;
+    }
+
+    public DataCell(CellContent cellContent, String borderLeftColor, int colspan, boolean withBorder, AlignmentType alignmentType) {
+        this.cellContent = cellContent;
+        this.borderLeftColor = borderLeftColor;
+        this.colspan = colspan;
+        this.withBorder = withBorder;
         this.alignmentType = alignmentType;
     }
 }
